42ab6d16229937157f3de37702532ffb26e7db73
rozanovz <rozanovz13@gmail.com>
2015-11-22 16:27:59 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name oceanmixApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the oceanmixApp
+ */
+angular.module('oceanmixApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
