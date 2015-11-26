49eb650b361a00f4d25bdabff54301409f08614a
Alex Rozanov <rozanovz13@gmail.com>
2015-11-26 17:15:29 +0200

    cleaned directory

--- app/scripts/plugins/navbar.js
+++
@@ -1,8 +0,0 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
-
-$(function(){
-  $('#menuStick').slicknav({
-      brand:"<img src=\"../images/rocket.svg\" alt=\"\">",
-      label:"Меню"
-    });
-});
