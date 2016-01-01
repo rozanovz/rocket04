eb021dc2d9114e74bbaa601889f080031b0d0399
rozanovz <rozanovz13@gmail.com>
2016-01-02 00:54:24 +0200

    some improvments added to the card

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -8,7 +8,20 @@
  * Controller of the ocean04App
  */
 angular.module('ocean04App')
-  .controller('cartCtrl', function ($scope, $rootScope) {
+  .controller('cartCtrl', function ($scope, $rootScope, ngCart) {
     $rootScope.store = true;
     $rootScope.description = true;
+
+    $scope.cartItems = ngCart.getCart();
+
+    $scope.getCart = function(){
+      $scope.cartTotal = ngCart.totalCost();
+    }
+
+    $scope.removeItem = function(id){
+      ngCart.removeItemById(id);
+    }
+
+    $scope.getCart();
+
   });
