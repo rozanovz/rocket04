2ade1364c7481cffd1a42d8cda1f587496c5e538
rozanovz <rozanovz13@gmail.com>
2016-01-05 16:58:04 +0200

    added angular integration

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -12,6 +12,16 @@ angular.module('ocean04App')
     $rootScope.store = true;
     $rootScope.description = true;
 
+    $scope.formUser = {};
+
+    $scope.checkShipping = function () {
+      if(ngCart.totalCost()>500){
+        $scope.shipping = 0;
+      }else{
+        $scope.shipping = 20;
+      }
+    }
+
     $scope.cartItems = ngCart.getCart();
 
     $scope.getCart = function(){
@@ -22,6 +32,24 @@ angular.module('ocean04App')
       ngCart.removeItemById(id);
     }
 
+    $scope.countTotal = function () {
+      $scope.checkShipping();
+      $scope.totalWithShipping = ngCart.totalCost() + $scope.shipping;
+    }
+
     $scope.getCart();
+    $scope.checkShipping();
+    $scope.countTotal();
+
+    $scope.checkout = function () {
+      var items = ngCart.getCart().items;
+      items.forEach(function (key) {
+        delete key._data;
+        delete key._id;
+        delete key.$$hashKey;
+      });
+      $scope.formUser.items = items;
+      console.log(JSON.stringify($scope.formUser));
+    }
 
   });
